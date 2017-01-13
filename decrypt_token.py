a1c62191975401d617a51fdfb835741b526e1363
vzagorovskiy <vzagorovskiy@dataplus.ru>
2017-01-13 11:05:34 +0300

    Update decrypt_token.py

--- decrypt_token.py
+++ decrypt_token.py
@@ -6,7 +6,7 @@ from base64 import *
 iv = '\x01' + '\x02' + '\x03' + '\x04' + '\x05' + '\x06' + '\x07' + '\x08' + '\x09' + '\x0A' + '\x0B' + '\x0C' + '\x0D' + '\x0E' + '\x0F' + '\x10'
 token = 'YOUR_TOKEN'
 if token[-1] <> '=':
-    token = token + '='
+    token += '='
 token = b64decode(token, '-_')
 mode = AES.MODE_CBC
 key = 'YOUR_SHARED_KEY'
