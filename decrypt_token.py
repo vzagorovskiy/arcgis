436886da48eaef02d1a36baf2b23e6322cd66d8c
vzagorovskiy <vzagorovskiy@dataplus.ru>
2017-02-01 15:16:18 +0300

    Replaced not equal operator

--- decrypt_token.py
+++ decrypt_token.py
@@ -1,11 +1,12 @@
 # Decrypt arcgis tokens
 # http://sainsb.github.io/2012/07/18/decrypting-arcgis-server-tokens/
-from Crypto.Cipher import AES
 from base64 import *
+
+from Crypto.Cipher import AES
 
 iv = '\x01' + '\x02' + '\x03' + '\x04' + '\x05' + '\x06' + '\x07' + '\x08' + '\x09' + '\x0A' + '\x0B' + '\x0C' + '\x0D' + '\x0E' + '\x0F' + '\x10'
 token = 'YOUR_TOKEN'
-if token[-1] <> '=':
+if token[-1] != '=':
     token += '='
 token = b64decode(token, '-_')
 mode = AES.MODE_CBC
