100dc5253efecfa6afd040d9ed24e9379ef53f3a
vzagorovskiy <vzagorovskiy@dataplus.ru>
2017-02-07 11:10:33 +0300

    added input shared_key and token

--- decrypt_token.py
+++ decrypt_token.py
@@ -2,25 +2,30 @@
 # http://sainsb.github.io/2012/07/18/decrypting-arcgis-server-tokens/
 # Python 2.7
 
-from base64 import *
-
+from base64 import b64decode
 from Crypto.Cipher import AES
+import logging
 
 
 def decrypt_token(token, shared_key):
-    iv = '\x01' + '\x02' + '\x03' + '\x04' + '\x05' + '\x06' + '\x07' + '\x08' + '\x09' + '\x0A' + '\x0B' + '\x0C' + '\x0D' + '\x0E' + '\x0F' + '\x10'
-    if token[-1] != '=': token += '='
-    token = b64decode(token, '-_')
-    mode = AES.MODE_CBC
-    # for portal https://domain.com/portal/portaladmin/security/tokens
-    # for server https://domain.com/arcgis/admin/security/tokens
-    key = shared_key[:16]
-    cipher = AES.new(key, mode, iv)
-    return cipher.decrypt(token)
+    try:
+        iv = '\x01' + '\x02' + '\x03' + '\x04' + '\x05' + '\x06' + '\x07' + '\x08' + '\x09' + '\x0A' + '\x0B' + '\x0C' + '\x0D' + '\x0E' + '\x0F' + '\x10'
+        if token[-1] != '=':
+            token += '='
+        token = b64decode(token, '-_')
+        mode = AES.MODE_CBC
+        key = shared_key[:16]
+        cipher = AES.new(key, mode, iv)
+        return cipher.decrypt(token)
+    except:
+        logging.exception('Error occured')
 
 
 if __name__ == '__main__':
+    print('Shared key for portal https://domain.com/portal/portaladmin/security/tokens')
+    print('Shared key for server https://domain.com/arcgis/admin/security/tokens')
     shared_key = raw_input('Shared key: ')
     token = raw_input('Token: ')
     info = decrypt_token(token, shared_key)
-    print(info)
+    if info is not None:
+        print(info)
